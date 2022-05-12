33703537b7230befd802f78c061c2991f2d611d7
ruuen <max@madebyruuen.com>
2022-05-12 20:18:56 +1000

    Add thank you msg screen, add form submit behaviour, add desktop layout
    Move card styling to the main wrapper element from individual card elements, remove card class
    
    Add max-width to wrapper instead of controlling it via mediaqry like I normally would
    
    In future I can probably replace the media queries used here with usage of clamp(), need to review further

--- site_dir/app.js
+++ site_dir/app.js
@@ -1,3 +1,4 @@
+const form = document.querySelector(".rating__options");
 const formBtns = document.querySelectorAll(".rating__options-btn");
 
 formBtns.forEach((btn) => {
@@ -7,12 +8,34 @@ formBtns.forEach((btn) => {
   });
 });
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const activeBtn = getActiveBtnValue();
+
+  if (!activeBtn) {
+    return;
+  }
+
+  showSubmitMsg(activeBtn);
+});
+
 const setActiveBtn = (btn) => {
   btn.classList.add("rating__options-btn--active");
+};
+
+const getActiveBtnValue = () => {
+  const activeBtn = document.querySelector(".rating__options-btn--active");
+  return activeBtn ? activeBtn.value : false;
 };
 
 const clearBtnSelection = () => {
   formBtns.forEach((btn) => {
     btn.classList.remove("rating__options-btn--active");
   });
+};
+
+const showSubmitMsg = (rating) => {
+  document.querySelector(".rating").classList.add("hidden");
+  document.querySelector(".thank-you").classList.remove("hidden");
+  document.querySelector(".thank-you__rating").innerText = `You selected ${rating} out of 5`;
 };
